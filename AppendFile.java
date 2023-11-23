d8a53c85d644e967f9494d52fd01c04b5de77574
Szilard Varga <136651660+szilard-varga@users.noreply.github.com>
2023-11-23 09:33:49 +0200

    Update AppendFile.java

--- AppendFile.java
+++ AppendFile.java
@@ -3,6 +3,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
+// just appending
 public class AppendFile {
 
     public static void main(String[] args) {
