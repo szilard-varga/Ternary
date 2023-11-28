94a37acdf41c61a47081119b0ce7f87a7b0ff9ee
Szilard Varga <136651660+szilard-varga@users.noreply.github.com>
2023-11-28 19:04:32 +0200

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -1,13 +1,16 @@
 import java.util.LinkedList;
 
+// this is an exmaple of linked list 
 class Main {
   public static void main(String[] args) {
     LinkedList<String> languages = new LinkedList<>();
 
-    // add elements in the LinkedList
+    // add test hardcoded elements in the LinkedList
     languages.add("Python");
     languages.add("Java");
     languages.add("JavaScript");
+
+    // print before 
     System.out.println("LinkedList: " + languages);
 
     // get the element from the LinkedList
