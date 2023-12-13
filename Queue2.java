a3a17322bc49e168c2ab99b080277232b54dfcf8
Szilard Varga <136651660+szilard-varga@users.noreply.github.com>
2023-12-13 10:31:02 +0200

    Update Queue2.java

--- Queue2.java
+++ Queue2.java
@@ -115,4 +115,23 @@ public class Queue {
     q.display();
 
   }
+
+  // test method
+  public static void loop() {
+
+    int n1 = 72, n2 = 120, lcm;
+
+    // maximum number between n1 and n2 is stored in lcm
+    lcm = (n1 > n2) ? n1 : n2;
+
+    // Always true
+    while(true) {
+      if( lcm % n1 == 0 && lcm % n2 == 0 ) {
+        System.out.printf("The LCM of %d and %d is %d.", n1, n2, lcm);
+        break;
+      }
+      ++lcm;
+    }
+  }
+  
 }
